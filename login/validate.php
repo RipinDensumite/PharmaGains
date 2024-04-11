dbc006048598da7f7654243425a929bae5b8600a
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-04-11 10:13:24 +0800

    Change users verifcation into array

--- login/validate.php
+++ login/validate.php
@@ -1,23 +1,17 @@
 <?php
 $username = $_POST["username"];
-$pswd = $_POST["password"];
-$validate = false;
+$password = $_POST["password"];
 
-do {
-	if ($username == "ahmad" && $pswd == "1234") {
-		$validate = true;
-		header("Location: /pharmagains");
-		exit();
-		break;
-	} elseif ($username == "thayyib" && $pswd == "4321") {
-		$validate = true;
-		header("Location: /pharmagains");
-		exit();
-		break;
-	}
+$validUsers = [
+    "ahmad" => "1234",
+    "thayyib" => "4321"
+];
 
-	$validate = false;
-	header("Location: index.html");
-	exit();
-} while ($validate == false)
+if (array_key_exists($username, $validUsers) && $validUsers[$username] === $password) {
+    header("Location: /pharmagains");
+    exit();
+} else {
+    header("Location: .");
+    exit();
+}
 ?>
