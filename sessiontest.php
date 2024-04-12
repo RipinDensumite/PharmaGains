6e5bc537cc4d846ee36508c1a4523c01360152e3
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-04-12 12:42:11 +0800

    Add session test php

---
+++ sessiontest.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if (isset($_SESSION["username"])){
+    echo $_SESSION["username"];
+}else{
+    echo "There's no session available";
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    session_unset();
+    session_destroy();
+    header("Location: /pharmagains");
+    exit;
+}
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+    <button type="submit">Logout</button>
+</form>
