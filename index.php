6db064198aaaf04c1809e171b522cf58b78f5bcf
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-05-07 17:18:17 +0800

    Add profile button

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "pharmagains";
+$prodNum = 0;
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -20,10 +21,12 @@ $result = $conn->query($sql);
 $products = array();
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
+        $prodNum++;
         $products[] = array(
             'image' => $row['image'],
             'name' => $row['name'],
-            'price' => $row['price']
+            'price' => $row['price'],
+            'product_id' => $row['product_id']
         );
     }
 }
@@ -45,6 +48,21 @@ if (isset($_GET['search']) && !empty($_GET['search'])) {
     $searchQuery = $_GET['search'];
     // Filter products based on the search query
     $products = filterProducts($products, $searchQuery);
+    $prodNum = count($products);
+}
+
+// Check if search form is submitted
+if (isset($_GET['product_id']) && !empty($_GET['product_id'])) {
+    $product_id = $_GET['product_id'];
+    $cart_id = $_SESSION['cart_id'];
+    require_once 'config/db.php';
+
+    $sqlConfig = new SqlConfig();
+
+    // Call the methods of the SqlConfig class
+    $sqlConfig->DatabaseChecker();
+    $sqlConfig->TableChecker();
+    $msg = $sqlConfig->AddToCart($cart_id, $product_id);
 }
 ?>
 
@@ -87,6 +105,9 @@ if (isset($_GET['search']) && !empty($_GET['search'])) {
             echo "<a href='endSession.php'>";
             echo    "<button>logout</button>";
             echo "</a>";
+            echo "<a href='profile'>";
+            echo    "<button>Profile</button>";
+            echo "</a>";
         }
         ?>
         <a href="register">
@@ -108,22 +129,35 @@ if (isset($_GET['search']) && !empty($_GET['search'])) {
     <h2 align="center">
         <font color="white">Pharma Gains Market</font>
     </h2>
+    <p align="center">
+        <font color="white">
+        <?php echo "total products : " . $prodNum; ?>
+        </font>
+    </p>
+    <p align="center">
+        <font color="white">
+        <?php 
+        if(isset($msg)){
+            echo $msg;
+        }
+         ?>
+        </font>
+    </p>
     <!-- Display filtered products -->
     <table align="center" border="4" bgcolor="#734FB7">
         <?php foreach ($products as $index => $product): ?>
             <?php if ($index % 3 === 0): ?>
                 <tr>
             <?php endif; ?>
-
             <td>
                 <div style="display: flex; flex-direction: column; align-items: center; gap: 10px;">
                     <img src="<?php echo $product['image']; ?>" width="200" height="200"/>
                     <span style="color: white; font-weight: bold;"><?php echo $product['name']; ?></span>
                     <span style="color: white; font-weight: bold;"><?php echo "RM " . $product['price']; ?></span>
-                    <div style="display:flex;flex-direction:column;width:100%;">
-                        <button>Buy Now</button>
-                        <button>Add to cart</button>
-                    </div>
+                    <form method="get" style="display:flex;flex-direction:column;width:100%;">
+                        <input type="hidden" name="product_id" value="<?php echo $product['product_id']; ?>">
+                        <button type="submit">Add to cart</button>
+                    </form>    
                 </div>
             </td>
             <?php if (($index + 1) % 3 === 0 || $index === count($products) - 1): ?>
