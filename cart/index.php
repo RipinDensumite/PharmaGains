f9cec94585fd0e738e10f99b941ed5e3a3018f9d
root <root@Ripin>
2024-10-01 03:46:18 +0800

    Redirect to login page if not login on cart page

--- cart/index.php
+++ cart/index.php
@@ -3,7 +3,7 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
-    header("Location: login.php");
+    header("Location: /login");
     exit;
 }
 
